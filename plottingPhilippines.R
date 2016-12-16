cae1215cbb4c5df11b24cd08745008a46b80ed23
Edie <ehze@live.com>
2016-12-16 10:25:45 -0800

    still wrangling all these data
    i.e. learning about the geography of the Philippines

--- plottingPhilippines.R
+++ plottingPhilippines.R
@@ -1,49 +1,54 @@
-setwd("/Users/edie/Box Sync/GitThings/philippineCrime")
+## ee
+# 2016-12
 
-## TRY 1
-# I wanted to see if I could go about using this method, but it did not zoom into the Philippines the way I would've preferred
-library(ggplot2)
-library(ggmap)
-
-pi_map = get_map("Philippines", maptype="toner-lite", zoom=6)
-ggmap(pi_map)
-
-
-
-## TRY 2
-# This is the route I will use
-# Data Source: http://www.gadm.org/country
+## RESOURCES:
+# Data: http://www.gadm.org/country
+# Tutorial: 
+# https://www.students.ncl.ac.uk/keith.newman/r/maps-in-r-using-gadm
 library(sp)
 
-# Here's a tutorial I'm reading through
-# https://www.students.ncl.ac.uk/keith.newman/r/maps-in-r-using-gadm
 
-# Level 0: The outline of the country (in the UK this is anywhere shaded in yellow)
+setwd("/Users/edie/Box Sync/GitThings/philippineCrime")
+
+
+## LEVEL 0
 level0 = readRDS("datasets/PHL_adm0.rds")
 plot(level0, main="Level 0", col="lightgrey", border="darkgrey")
 
-# Level 1: Major borders in the country, marking the boundaries between England, Scotland, Wales and Northern Ireland (in the UK map these are the red lines)
-level1 = readRDS("PHL_adm1.rds")
+
+
+## LEVEL 1
+level1 = readRDS("datasets/PHL_adm1.rds")
 plot(level1, main="Level 1", col="lightgrey", border="darkgrey", xlim=c(115,130), ylim=c(5,20))
 
 names(level1)
-head(level1$NAME_0) # just PI
+head(level1$NAME_0) # Nation: Philippines
 head(level1$NAME_1) # name of counties?
-# These are actually provinces
-# https://en.wikipedia.org/wiki/Quezon
 
 
-
-# Level 2: Boundaries of counties
-# boroughs and districts in the UK image are the blue lines
+## LEVEL 2
 level2 = readRDS("datasets/PHL_adm2.rds")
 plot(level2, main="Level 2")
 
 names(level2)
-head(level2$NAME_0) # just PI
-head(level2$NAME_1) # these are all provinces
-head(level2$NAME_2) # these are all cities
+head(level2$NAME_0) # Nation: Philippines
+
+head(level2$NAME_1)
+sample(level2$NAME_1, 10) # Provinces
+
+head(level2$NAME_2)
+sample(level2$NAME_2, 10) # Cities
+
 
 # Level 3: Seems to be pretty black -- no documentation on this
 level3 = readRDS("datasets/PHL_adm3.rds")
 plot(level3, main="Level 3")
+
+names(level3)
+head(level3$NAME_0) # Philippines
+
+head(level3$NAME_1) # Provinces(?)
+sample(level3$NAME_1, 10)
+
+head(level3$NAME_2)
+head(level3$NAME_3)
--- understandingPopData.R
+++ understandingPopData.R
@@ -3,35 +3,6 @@
 ## LOADING IN DATA
 population = read.csv("datasets/population2015.csv")
 
-# # cleaning it up
-# names(population)
-# population = cbind(population[,1:4], population[,6:7])
-# names(population)[5:6] = c("Area","Density")
-# names(population) = tolower(names(population))
-# 
-# attach(population)
-# 
-# levels(region)
-
-
-# # cleaning it up again
-# names(population)
-# names(population)[5:6] = c("Area","Density")
-# names(population) = tolower(names(population))
-# attach(population)
-# levels(region) # still need to remove excess rows
-
-
-# cleaning it up hopefully the last time
-# names(population)
-# names(population)[5:6] = c("Area","Density")
-# names(population) = tolower(names(population))
-# attach(population)
-# levels(region)
-# which(region=="")
-# length(region)
-# city[1516] # Tandag City, Region XIII - Caraga # Fixed
-
 attach(population)
 names(population)
 
@@ -39,6 +10,7 @@ names(population)
 ## START THE DATA CLEANING
 # let's find something cool, yeah?
 all_cities = city # Multiple city names are repeated, obvi
+# Or is this unambiguously obvi?
 
 
 ## FIX DENSITY TO NUMERIC
