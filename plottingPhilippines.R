d764c09b646de3dda87758186543b6584f6af3e2
Edie <ehze@live.com>
2016-12-16 10:42:40 -0800

    found out the levels in the gadm data

--- plottingPhilippines.R
+++ plottingPhilippines.R
@@ -26,6 +26,7 @@ head(level1$NAME_0) # Nation: Philippines
 head(level1$NAME_1) # name of counties?
 
 
+
 ## LEVEL 2
 level2 = readRDS("datasets/PHL_adm2.rds")
 plot(level2, main="Level 2")
@@ -40,6 +41,7 @@ head(level2$NAME_2)
 sample(level2$NAME_2, 10) # Cities
 
 
+
 # Level 3: Seems to be pretty black -- no documentation on this
 level3 = readRDS("datasets/PHL_adm3.rds")
 plot(level3, main="Level 3")
@@ -47,8 +49,11 @@ plot(level3, main="Level 3")
 names(level3)
 head(level3$NAME_0) # Philippines
 
-head(level3$NAME_1) # Provinces(?)
-sample(level3$NAME_1, 10)
+head(level3$NAME_1)
+sample(level3$NAME_1, 10) # Regions
 
 head(level3$NAME_2)
+sample(level3$NAME_2, 10) # Cities
+
 head(level3$NAME_3)
+sample(level3$NAME_3, 10) # Barangay = District
